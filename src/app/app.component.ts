891f1dbe41fdc21fe399693e24788886eff6f21f
Ismael <ismael.j.jurado@accenture.com>
2024-04-10 17:26:01 +0200

    commit base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
+@Component({ 
+  selector: 'app-component',
   template: `
     <h1>Hello World</h1>
   `
 })
 export class AppComponent {
+  
+  constructor() {
 
+  }
+  
 }
